da34096bacf2ae2b2b6e015b40e46dd13f276dda
salihsungurcelik <salihcelik099@gmail.com>
2024-09-05 21:23:21 +0300

    v2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { baseImgURL, options } from "../constant";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import { Link } from "react-router-dom";
 
 const MovieList = ({ genre }) => {
-  // gelen proptaki kategorinin bilgisine göre
+  const [movies, setMovies] = useState(null);
+  // gelen proptaki kategorinin id'sine bilgisine göre
   // kategoriye ait film verisini çekip
   // bunları slider içerisinde listeleme
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/discover/movie?with_genres=${genre.id}`,
+        options
+      )
+      .then((res) => setMovies(res.data.results));
+  });
   return (
-    <div>
-      <h1>{genre.name} </h1>
+    <div className="p-4">
+      <h1 className="mb-3">{genre.name} </h1>
+      <Splide
+        options={{
+          // rewind: true,
+          loop: true,
+          gap: "10px",
+          autoWidth: true,
+          pagination: false,
+        }}
+      >
+        {movies?.map((movie) => (
+          <SplideSlide>
+            <Link to={`/detay/${movie.id}`}>
+              <img className="movie" src={baseImgURL + movie.poster_path} />
+            </Link>
+          </SplideSlide>
+        ))}
+      </Splide>
     </div>
   );
 };
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,7 +1,99 @@
-const DetailPage = () => {
-  return (
-    <div>DetailPage</div>
-  )
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { baseImgURL, options } from "../constant";
+import millify from "millify";
 
-export default DetailPage
+const DetailPage = () => {
+  const [movie, setMovie] = useState(null);
+  // 1) url'den filmin id'sini al
+  const { id } = useParams();
+
+  // 2) film verisini api'den al
+  useEffect(() => {
+    axios
+      .get(
+        `/movie/${id}?append_to_response=credits%2Cvideos&language=tr-TR`,
+        options
+      )
+      .then((res) => setMovie(res.data));
+  }, []);
+
+  // 3) oyuncuların verisini al
+  return (
+    <div className="row">
+      {!movie ? (
+        <div class="spinner-border text-primary" role="status"></div>
+      ) : (
+        <>
+          {/* Üst alan */}
+          <div className="col-12 banner ">
+            <img
+              className="w-100 h-100 object-fit-cover"
+              src={baseImgURL + movie.backdrop_path}
+            />
+            <div className="banner-bg">
+              <span>{movie.title} </span>
+            </div>
+          </div>
+          {/* sol taraf */}
+          <div className="col-md-6 mt-4 p-4">
+            {/* 1 Şirketler */}
+            <h3>Yapımcı Şirketler</h3>
+            <div className="d-flex flex-wrap gap-4">
+              {movie.production_companies.map((i) => (
+                <div className="bg-white rounded p-2 d-flex align-items-center">
+                  {i.logo_path ? (
+                    <img
+                      className="object-fit-cover"
+                      width={100}
+                      height={50}
+                      src={baseImgURL + i.logo_path}
+                      alt=""
+                    />
+                  ) : (
+                    <span className="company">{i.name}</span>
+                  )}
+                </div>
+              ))}
+            </div>
+            {/* 2 Diller */}
+            <h3 className="mt-4">Konuşulan Diller</h3>
+            <div className="d-flex flex-wrap gap-4">
+              {movie.spoken_languages.map((i) => (
+                <div className="bg-white rounded p-2 d-flex align-items-center">
+                  <span className="company">{i.name}</span>
+                </div>
+              ))}
+            </div>
+            {/* 3 Ülkeler */}
+            <h3 className="mt-4">Yapımcı Ülke</h3>
+            <div className="d-flex flex-wrap gap-4">
+              {movie.production_countries.map((i) => (
+                <div className="bg-white rounded p-2 d-flex align-items-center">
+                  <span className="company">{i.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Sağ taraf */}
+          <div className="col-md-6 mt-4 p-4">
+            <p className="lead">{movie.overview}</p>
+            <p className="fs-5">
+              <span>Bütçe: </span>
+              <span className="text-success">{millify(movie.budget)}$</span>
+            </p>
+
+            <p className="fs-5">
+              <span>Gelir: </span>
+              <span className="text-success">{millify(movie.revenue)}$</span>
+            </p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DetailPage;
