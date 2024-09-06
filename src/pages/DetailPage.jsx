a1f3f08209f37ac0f051e3291bc3026d42a732f5
salihsungurcelik <salihcelik099@gmail.com>
2024-09-06 20:16:16 +0300

    finish

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { baseImgURL, options } from "../constant";
 import millify from "millify";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import ReactPlayer from "react-player";
 
 const DetailPage = () => {
   const [movie, setMovie] = useState(null);
@@ -27,7 +29,7 @@ const DetailPage = () => {
       ) : (
         <>
           {/* Üst alan */}
-          <div className="col-12 banner ">
+          <div className="col-12 banner">
             <img
               className="w-100 h-100 object-fit-cover"
               src={baseImgURL + movie.backdrop_path}
@@ -36,59 +38,117 @@ const DetailPage = () => {
               <span>{movie.title} </span>
             </div>
           </div>
-          {/* sol taraf */}
-          <div className="col-md-6 mt-4 p-4">
-            {/* 1 Şirketler */}
-            <h3>Yapımcı Şirketler</h3>
-            <div className="d-flex flex-wrap gap-4">
-              {movie.production_companies.map((i) => (
-                <div className="bg-white rounded p-2 d-flex align-items-center">
-                  {i.logo_path ? (
-                    <img
-                      className="object-fit-cover"
-                      width={100}
-                      height={50}
-                      src={baseImgURL + i.logo_path}
-                      alt=""
-                    />
-                  ) : (
+          <div className="container p-4 p-md-5">
+            {/* sol taraf */}
+            <div className="col-md-6 mt-4">
+              {/* 1 Şirketler */}
+              <h3>Yapımcı Şirketler</h3>
+              <div className="d-flex flex-wrap gap-4">
+                {movie.production_companies.map((i) => (
+                  <div className="bg-white rounded p-2 d-flex align-items-center">
+                    {i.logo_path ? (
+                      <img
+                        className="object-fit-cover"
+                        width={100}
+                        height={50}
+                        src={baseImgURL + i.logo_path}
+                        alt=""
+                      />
+                    ) : (
+                      <span className="company">{i.name}</span>
+                    )}
+                  </div>
+                ))}
+              </div>
+              {/* 2 Diller */}
+              <h3 className="mt-4">Konuşulan Diller</h3>
+              <div className="d-flex flex-wrap gap-4">
+                {movie.spoken_languages.map((i) => (
+                  <div className="bg-white rounded p-2 d-flex align-items-center">
                     <span className="company">{i.name}</span>
-                  )}
-                </div>
-              ))}
+                  </div>
+                ))}
+              </div>
+              {/* 3 Ülkeler */}
+              <h3 className="mt-4">Yapımcı Ülke</h3>
+              <div className="d-flex flex-wrap gap-4">
+                {movie.production_countries.map((i) => (
+                  <div className="bg-white rounded p-2 d-flex align-items-center">
+                    <span className="company">{i.name}</span>
+                  </div>
+                ))}
+              </div>
             </div>
-            {/* 2 Diller */}
-            <h3 className="mt-4">Konuşulan Diller</h3>
-            <div className="d-flex flex-wrap gap-4">
-              {movie.spoken_languages.map((i) => (
-                <div className="bg-white rounded p-2 d-flex align-items-center">
-                  <span className="company">{i.name}</span>
-                </div>
-              ))}
-            </div>
-            {/* 3 Ülkeler */}
-            <h3 className="mt-4">Yapımcı Ülke</h3>
-            <div className="d-flex flex-wrap gap-4">
-              {movie.production_countries.map((i) => (
-                <div className="bg-white rounded p-2 d-flex align-items-center">
-                  <span className="company">{i.name}</span>
-                </div>
-              ))}
-            </div>
-          </div>
 
-          {/* Sağ taraf */}
-          <div className="col-md-6 mt-4 p-4">
-            <p className="lead">{movie.overview}</p>
-            <p className="fs-5">
-              <span>Bütçe: </span>
-              <span className="text-success">{millify(movie.budget)}$</span>
-            </p>
+            {/* Sağ taraf */}
+            <div className="col-md-6 mt-4">
+              <p className="lead">{movie.overview}</p>
+              <p className="fs-5">
+                <span>Bütçe: </span>
+                <span className="text-success">{millify(movie.budget)}$</span>
+              </p>
 
-            <p className="fs-5">
-              <span>Gelir: </span>
-              <span className="text-success">{millify(movie.revenue)}$</span>
-            </p>
+              <p className="fs-5">
+                <span>Gelir: </span>
+                <span className="text-success">{millify(movie.revenue)}$</span>
+              </p>
+            </div>
+
+            {/* oyuncuların listesi */}
+            <div className="col-12 my-3">
+              <h2>Oyuncular</h2>
+              <Splide
+                options={{
+                  height: "200px",
+                  gap: "10px",
+                  pagination: false,
+                  autoWidth: true,
+                }}
+              >
+                {movie.credits.cast.map((i) => (
+                  <SplideSlide>
+                    <div className="actor-card h-100">
+                      <img
+                        className="movie"
+                        src={
+                          i.profile_path
+                            ? baseImgURL + i.profile_path
+                            : "/profil.jpg"
+                        }
+                      />
+                      <p>
+                        <span>{i.character} </span>
+                        <span>{i.name}</span>
+                      </p>
+                    </div>
+                  </SplideSlide>
+                ))}
+              </Splide>
+            </div>
+
+            {/* videolar */}
+            <div className="my-5">
+              <h1>Videolar</h1>
+              <Splide
+                options={{
+                  height: "50vh",
+                }}
+              >
+                {movie.videos.results.map(
+                  (video) =>
+                    video.site === "YouTube" && (
+                      <SplideSlide>
+                        <ReactPlayer
+                          width={"100%"}
+                          height={"100%"}
+                          key={video.id}
+                          url={`https://www.youtube.com/watch?v=${video.key}`}
+                        />
+                      </SplideSlide>
+                    )
+                )}
+              </Splide>
+            </div>
           </div>
         </>
       )}
